9fdd85dd686087df79faf61d9d401a2c8f69e832
Jecsan <Jecsanb@outlook.com>
2020-05-25 17:51:07 -0500

    implemented simple search,added new classes for users and group

--- src/ezadmintool/active_directory.py
+++ src/ezadmintool/active_directory.py
@@ -1,4 +1,9 @@
+from enum import Enum
+
 from ldap3 import Server, Connection, ALL
+from ldap3.utils.conv import escape_filter_chars
+
+from ezadmintool.config_management import ConfigurationManagement
 
 
 class Entity:
@@ -19,6 +24,23 @@ class Entity:
 
 
 class User(Entity):
+    __GENERAL_ATTRIBUTES = {
+        'givenName': 'First Name',
+        'initials': 'Initials',
+        'sn': 'Last Name',
+        'displayName': 'Display Name',
+        'description': 'Description',
+        'physicalDeliveryOfficeName': 'Office',
+        'telephoneNumber': 'Telephone Number',
+        'mail': 'Email Address',
+        'wWWHomePage': 'Web Page',
+        'CN': 'Common Name',
+    }
+
+    @staticmethod
+    def general_attributes(include_friendly_name=False):
+        return list(User.__GENERAL_ATTRIBUTES if include_friendly_name else User.__GENERAL_ATTRIBUTES.keys())
+
     def lock(self):
         pass
 
@@ -47,22 +69,30 @@ class Group(Entity):
 
     pass
 
+    @classmethod
+    def general_attributes(cls):
+        pass
+
 
 class ADConfiguration:
+
     def __init__(self, config=None):
         self.HOST_NAME = None
         self.PORT = None
         self.USERNAME = None
         self.PASSWORD = None
-
         if config is None:
             config = dict()
-        self.required = ['HOST_NAME']
-        self.optional = ['PORT', 'USERNAME', 'PORT']
+        self.required = ['host_name']
+        self.optional = ['port', 'username', 'port', 'password']
         for attribute in (self.required + self.optional):
             if attribute in self.required and attribute not in config:
-                raise ValueError(f"Missing attribute : {attribute}")
-            setattr(self, attribute, config[attribute] if attribute in config else None)
+                raise ValueError(f'Missing attribute : {attribute}')
+            setattr(self, attribute.upper(), config[attribute] if attribute in config else None)
+        self.correct_types()
+
+    def correct_types(self):
+        self.PORT = int(self.PORT)
 
     def __str__(self):
         str = ''
@@ -76,6 +106,9 @@ class ADConnection:
         self.server = Server(host=config.HOST_NAME, port=config.PORT, get_info=ALL)
         self.connection = Connection(self.server, user=config.USERNAME, password=config.PASSWORD)
         self.connected = False
+
+    def search(self, base=None, filter_string='(objectClass=*)'):
+        return self.connection.search(base, filter_string)
 
     def connect(self):
         self.connected = self.connection.bind()
@@ -94,18 +127,58 @@ class ActiveDirectory:
     def __init__(self, config):
         self.config = ADConfiguration(config)
         self.connection = ADConnection(self.config)
+        self.connection.connect()
 
-    pass
+    class SearchType(Enum):
+        USER = 0
+        GROUP = 1
+        ALL = 2
 
-    def add_entity(self, entity, copy=None, container_path=None, template=None):
+    def add(self, entity, copy=None, container_path=None, template=None):
         pass
 
-    def find_entity(self):
-        pass
+    def user_search_filter(self):
+        return
+
+    def find(self, name, search_type=ALL):
+        attributes = None
+        if search_type == self.SearchType.USER:
+            search_filter = self.SearchFilter.user_search_filter(name)
+            attributes = User.general_attributes()
+        elif search_type == self.SearchType.GROUP:
+            search_filter = self.SearchFilter.group_search_filter(name)
+            attributes = Group.general_attributes()
+        else:
+            search_filter = self.SearchFilter.all_search_filter(name)
+        self.connection.connection.search(self.connection.server.info.naming_contexts[0], search_filter,
+                                          attributes=attributes)
+        return self.connection.connection.entries
+
+    class SearchFilter:
+        @classmethod
+        def user_search_filter(cls, name):
+            return '(&(objectClass=user)(|(userPrincipalName={0})(sAMAccountName={0})(uid={0})(mail={0})' \
+                   '(distinguishedName={0})(proxyAddresses=SMTP:{0})))'.format(
+                escape_filter_chars(name))
+
+        @classmethod
+        def group_search_filter(cls, name):
+            return '(&(objectClass=Group)(|(cn={0})(distinguishedName={0})))'.format(
+                escape_filter_chars(name))
+
+        @classmethod
+        def all_search_filter(cls, name):
+            return '(&(objectClass=*)(cn:={0}))'.format(
+                escape_filter_chars(name))
+
 
 def main():
-    config = ADConfiguration({'HOST_NAME': 'AD_001'})
-    print(config)
+    configurations = ConfigurationManagement()
+    server_conf = dict(configurations.active_configs['SERVER'])
+    auth_conf = dict(configurations.active_configs['AUTHENTICATION'])
+    ad = ActiveDirectory({**server_conf, **auth_conf})
+    result = ad.find('Cert Publishers')
+    print(result)
 
 
 if __name__ == '__main__':
